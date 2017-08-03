77077f8067adf6f6ca9c5caac9cac0ebc9f71873
thiagogol <goveiat@gmail.com>
2017-08-03 03:03:46 -0300

    fim metodo fem2d

--- Code/Boylestad_10_4/fem2d.m
+++ Code/Boylestad_10_4/fem2d.m
@@ -32,25 +32,24 @@ G = matG(C, t, qtdP, qtdT);
 [G, b] = setContorno(G, x);
 
 %Direto
-%sol = G\b;
+sol = G\b;
 
 
 %CG
-chute = zeros(length(b),1);
-M = length(b);
-sol = cg(G, chute, b, eye(M), 1000, 0.001);
+% chute = zeros(length(b),1);
+% M = length(b);
+% sol = cg(G, chute, b, eye(M), 1000, 0.001);
 
 
 
 x(x == -1) = sol;
 
 toc;
-pause;
 
 % %% Impressões
 % %Malha
-% figure
-% pdeplot(m)
+%  figure
+%  pdeplot(m)
 % hold on
 % scatter(pts(1,:),pts(2,:), 5, 'FaceColor','r')
 % 
@@ -62,12 +61,12 @@ for k = 1:8
     s{k} = p(:, p(1,:) <= cx{k}(2) & p(1,:) >= cx{k}(1) & p(2,:) >= cy{k}(1) & p(2,:) <= cy{k}(2));
 end
 S = [s{1} s{2} s{3} s{4} s{5} s{6} s{7} s{8}];
-figure
+fig = figure
 pdeplot(m,'XYData',x','ColorMap','jet')
 % hold on
 % pdeplot(m)
 hold on
-scatter(S(1,:),S(2,:), 15)
+scatter(S(1,:),S(2,:), 15, 'MarkerEdgeColor', 'k')
 % 
 % %Vetor Campo Elétrico
 % figure
@@ -84,7 +83,11 @@ scatter(S(1,:),S(2,:), 15)
 % figure
 % pdeplot(m,'XYData',x,'ZData',x, 'ColorMap','jet')
 
-
+set(fig,'Units','Inches');
+    pos = get(fig,'Position');
+    set(fig,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+    nmFig = ['_0.2' '.pdf'];
+    print(fig,nmFig,'-dpdf','-r0')
 
 
 
